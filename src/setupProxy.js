4673faac06ac6edd199a0b763c799ed3e59c6a9e
KirShein71 <103045855+KirShein71@users.noreply.github.com>
2023-08-15 12:56:26 +0300

    fix

--- src/setupProxy.js
+++ src/setupProxy.js
@@ -1,11 +1,11 @@
 const { createProxyMiddleware } = require('http-proxy-middleware');
 
-module.exports = function(app) {
+module.exports = function (app) {
   app.use(
     '/api',
     createProxyMiddleware({
-      target: 'http://app.ruswine-spb.ru',
+      target: 'https://app.ruswine-spb.ru',
       changeOrigin: true,
-    })
+    }),
   );
 };
