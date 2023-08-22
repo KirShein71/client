21cf1a2a6c8be534e562a5f1212e55d7e73983c3
KirShein71 <103045855+KirShein71@users.noreply.github.com>
2023-08-22 16:20:48 +0300

    fix

--- src/components/Promo.jsx
+++ src/components/Promo.jsx
@@ -1,82 +1,18 @@
 import React from 'react';
-import PromoList from './PromoList';
-import Brand from '../components/Brand';
-import Loading from '../components/Loading';
-import { Link } from 'react-router-dom';
-import { AppContext } from './AppContext';
 import { observer } from 'mobx-react';
-import { fetchBrands, fetchPromoProducts } from '.././http/productApi';
-import { useLocation, useSearchParams } from 'react-router-dom';
-
-const getSearchParams = (searchParams) => {
-  let brand = searchParams.get('brand');
-  if (brand && /[1-9][0-9]*/.test(brand)) {
-    brand = parseInt(brand);
-  }
-  return { brand };
-};
 
 const Promo = observer(() => {
-  const { product } = React.useContext(AppContext);
-  const [brandsFetching, setBrandsFetching] = React.useState(true);
-  const [productFetching, setProductFetching] = React.useState(true);
-
-  const location = useLocation();
-  const [searchParams] = useSearchParams();
-
-  React.useEffect(() => {
-    fetchBrands()
-      .then((data) => (product.brands = data))
-      .finally(() => setBrandsFetching(false));
-
-    const { brand } = getSearchParams(searchParams);
-    product.brand = brand;
-
-    fetchPromoProducts(product.brand)
-      .then((data) => {
-        const filteredProducts = data.rows.filter((item) => item.brandId > 0);
-        product.products = filteredProducts;
-        product.count = filteredProducts.length;
-      })
-      .finally(() => setProductFetching(false));
-    // eslint-disable-next-line
-  }, []);
-
-  React.useEffect(() => {
-    const { brand } = getSearchParams(searchParams);
-    if (brand) {
-      if (brand !== product.brand) {
-        product.brand = brand;
-      }
-    } else {
-      product.brand = null;
-    }
-    // eslint-disable-next-line
-  }, [location.search]);
-
-  React.useEffect(() => {
-    setProductFetching(true);
-    fetchPromoProducts(product.brand)
-      .then((data) => {
-        const filteredProducts = data.rows.filter((item) => item.brandId > 0);
-        product.products = filteredProducts;
-        product.count = filteredProducts.length;
-      })
-      .finally(() => setProductFetching(false));
-  }, [product.brand, product]);
   return (
     <div className="promo">
       <div className="container">
-        <div className="promo-category">
-          <div>{brandsFetching ? <Loading /> : <Brand />}</div>
-          <Link to="/catalog">
-            <div className="promo__link">Весь каталог</div>
-          </Link>
+        <h1 className="promo__title">В этом месяце их можно забрать по цене закупки</h1>
+        <div className="promo__content">
+          <img src="./img/sela/5.jpg" alt="sale" />
+          <img src="./img/sela/2.jpg" alt="sale" />
+          <img src="./img/sela/3.jpg" alt="sale" />
+          <img src="./img/sela/4.jpg" alt="sale" />
+          <img src="./img/sela/1.jpg" alt="sale" />
         </div>
-        <div className="promo-content">{productFetching ? <Loading /> : <PromoList />}</div>
-        <Link to="/catalog">
-          <button className="promo__button">Все вина</button>
-        </Link>
       </div>
     </div>
   );
--- src/setupProxy.js
+++ src/setupProxy.js
@@ -4,7 +4,7 @@ module.exports = function (app) {
   app.use(
     '/api',
     createProxyMiddleware({
-      target: 'https://app.ruswine-spb.ru',
+      target: 'https://app.ruswine-spb.ru/',
       changeOrigin: true,
     }),
   );
