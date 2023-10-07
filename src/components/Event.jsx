13cca4dfc811de7bd27bbf88233bcc1c496c6f7a
KirShein71 <103045855+KirShein71@users.noreply.github.com>
2023-10-07 10:55:38 +0300

    fix

--- src/components/Event.jsx
+++ src/components/Event.jsx
@@ -23,6 +23,7 @@ function Event() {
         <div className="event__content">
           <EventToday />
           <div className="event-all">
+            <h2 className="event-all__title-mobile">Проведем для вас частное мероприятие</h2>
             <div className="event-all__content">
               {events.map((event, index) => (
                 <React.Fragment key={index}>
